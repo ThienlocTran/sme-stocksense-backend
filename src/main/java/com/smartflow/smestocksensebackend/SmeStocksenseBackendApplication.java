d100689bf5860ff63e89b1861004a140183e89fb
ThienlocTran <tranthienloc21102005@gmail.com>
2026-05-29 11:10:05 +0700

    Set up project base

---
+++ src/main/java/com/smartflow/smestocksensebackend/SmeStocksenseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartflow.smestocksensebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmeStocksenseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmeStocksenseBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smartflow/smestocksensebackend/SmeStocksenseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartflow.smestocksensebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmeStocksenseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

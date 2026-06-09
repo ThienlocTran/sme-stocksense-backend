7ffa0b21575ee3e4433b85188b63cc2ffbbd1118
ThienlocTran <tranthienloc21102005@gmail.com>
2026-06-09 23:39:08 +0700

    feat(T15): add employee lock unlock api

--- src/main/java/com/smartflow/smestocksensebackend/controller/EmployeeController.java
+++ src/main/java/com/smartflow/smestocksensebackend/controller/EmployeeController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -40,6 +41,16 @@ public class EmployeeController {
             @Valid @RequestBody UpdateEmployeeRequest request
     ) {
         return employeeService.updateEmployee(id, request);
+    }
+
+    @PatchMapping("/{id}/lock")
+    public EmployeeListItemResponse lockEmployee(@PathVariable Long id) {
+        return employeeService.lockEmployee(id);
+    }
+
+    @PatchMapping("/{id}/unlock")
+    public EmployeeListItemResponse unlockEmployee(@PathVariable Long id) {
+        return employeeService.unlockEmployee(id);
     }
 
     @GetMapping

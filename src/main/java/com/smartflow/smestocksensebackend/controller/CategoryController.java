eb928c3ee89c4e3e028f35a1445d77069a6e0614
Tonfa137206 <lequoctoan137@gmail.com>
2026-06-10 17:51:13 +0700

    feat(category): hoàn thành T24 API thêm mới danh mục

--- src/main/java/com/smartflow/smestocksensebackend/controller/CategoryController.java
+++ src/main/java/com/smartflow/smestocksensebackend/controller/CategoryController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/categories")
 @RequiredArgsConstructor

5774104292176d5b68311d5e162b967d43acd145
ThienlocTran <tranthienloc21102005@gmail.com>
2026-06-10 08:17:38 +0700

    feat(T25): add update category api

--- src/main/java/com/smartflow/smestocksensebackend/controller/CategoryController.java
+++ src/main/java/com/smartflow/smestocksensebackend/controller/CategoryController.java
@@ -3,16 +3,19 @@ package com.smartflow.smestocksensebackend.controller;
 import com.smartflow.smestocksensebackend.dto.category.CategoryListItemResponse;
 import com.smartflow.smestocksensebackend.dto.category.CategoryPageResponse;
 import com.smartflow.smestocksensebackend.dto.category.CreateCategoryRequest;
+import com.smartflow.smestocksensebackend.dto.category.UpdateCategoryRequest;
 import com.smartflow.smestocksensebackend.service.CategoryService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -29,6 +32,14 @@ public class CategoryController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(categoryService.createCategory(request));
+    }
+
+    @PutMapping("/{id}")
+    public CategoryListItemResponse updateCategory(
+            @PathVariable Long id,
+            @Valid @RequestBody UpdateCategoryRequest request
+    ) {
+        return categoryService.updateCategory(id, request);
     }
 
     @GetMapping
